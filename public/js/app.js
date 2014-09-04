120fd6c578ee35c7b6278b99baab5f283e3abdb9
shash7 <shashwat.amin@yahoo.com>
2014-09-04 20:28:33 +1000

    http server now working
    Added manual methods to serve css and js

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+
+/* jslint undef: true */
+/* global window, document, $ */
+
+/* ----------------------------------------------------------------
+ * app.js
+ * 
+ * Main js file for riki
+ * ---------------------------------------------------------------- */
+
+
+(function() {
+	
+	'use strict';
+	
+	function init() {
+		var all = document.getElementsByTagName('h1')[0];
+		all.style.color = '#f30';
+	}
+	
+	init();
+	
+})();
