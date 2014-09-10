44ad12ecd7b189062c123a41206311ab9f657d26
shash7 <shashwat.amin@yahoo.com>
2014-09-10 12:30:56 +1000

    Fixed some http bugs

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,43 @@
 	
 	'use strict';
 	
+	var x;
+	function getLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
+		} else {
+			console.log("Geolocation is not supported by this browser.");
+		}
+	}
+	function isInCircle(objPoint, objCircle, r) {
+		var px = objPoint.x;
+		var py = objPoint.y;
+		var cx = objCircle.x;
+		var cy = objCircle.y;
+		
+		var result = Math.sqrt(Math.pow(cx - px, 2) + Math.pow(cy - py, 2));
+		return result <= r;
+	}
+		
+	function showPosition(position) {
+		x.innerHTML = "Latitude: " + position.coords.latitude + 
+		"<br>Longitude: " + position.coords.longitude +
+			"<br>Accuracy: " + position.coords.accuracy;
+		var objCircle = {
+			x : position.coords.latitude,
+			y : position.coords.longitude
+		};
+		
+		var objPoint = {
+			x : 149,
+			y : -38
+		};
+		console.log(isInCircle(objPoint, objCircle, 300));
+	}
+	
 	function init() {
+		x = document.getElementsByClassName('sidebar')[0];
+		getLocation();
 	}
 	
 	init();
